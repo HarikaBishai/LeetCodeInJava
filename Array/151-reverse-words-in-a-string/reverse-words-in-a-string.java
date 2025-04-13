77ad31425e1132f26afd5104376c81f2b8db0c7d
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-12 21:11:21 -0400

    Time: 8 ms (39.73%) | Memory: 43.7 MB (32.76%) - LeetSync

---
+++ Array/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        List<String> words = Arrays.stream(s.split(" ")).filter(word -> !word.isEmpty()).collect(Collectors.toList());
+
+        Collections.reverse(words);
+
+        
+
+        
+        return String.join(" ", words);
+
+    }
+}
