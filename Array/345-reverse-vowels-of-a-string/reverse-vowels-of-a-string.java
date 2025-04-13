e9f2d370bb34d2f7d7df048c949165d5d36c4ec1
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-12 20:45:46 -0400

    Time: 5 ms (38.68%) | Memory: 45.3 MB (27.18%) - LeetSync

--- Array/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ Array/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,28 +1,52 @@
 class Solution {
     public String reverseVowels(String s) {
-        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')) ;
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U') ;
 
 
-        StringBuilder out = new StringBuilder(s);
-        int r = out.length()-1;
+        // StringBuilder out = new StringBuilder(s);
+        // int r = out.length()-1;
+        // int l = 0;
+
+        // while(l<r) {
+        //     char leftChar = out.charAt(l);
+        //     char rightChar = out.charAt(r);
+        //     if(!vowels.contains(leftChar)) {
+        //         l++;
+        //     } else if(!vowels.contains(rightChar)) {
+        //         r--;
+        //     } else {
+        //         out.setCharAt(l,rightChar);
+        //         out.setCharAt(r, leftChar);
+        //         l++;
+        //         r--;
+        //     }
+           
+        // }
+
+        // return out.toString();
+
+
+        char[] word = s.toCharArray();
+        int r = word.length-1;
         int l = 0;
 
         while(l<r) {
-            char leftChar = out.charAt(l);
-            char rightChar = out.charAt(r);
-            if(!vowels.contains(leftChar)) {
+            
+            if(!vowels.contains(word[l])) {
                 l++;
-            } else if(!vowels.contains(rightChar)) {
+            } else if(!vowels.contains(word[r])) {
                 r--;
             } else {
-                out.setCharAt(l,rightChar);
-                out.setCharAt(r, leftChar);
+                char temp = word[l];
+                word[l] = word[r];
+                word[r] = temp;
                 l++;
                 r--;
             }
            
         }
 
-        return out.toString();
+        return new String(word);
+
     }
 }
