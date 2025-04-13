add5145df89db72c295257150e77fc3f27d4e060
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-13 17:53:06 -0400

    Time: 41 ms (26.81%) | Memory: 48.1 MB (85.69%) - LeetSync

--- HashMap/2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ HashMap/2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -4,23 +4,6 @@ class Solution {
         int n = grid.length;
 
         int count = 0;
-        // for (int i=0;i<n;i++) {
-        //     int[] row = grid[i];
-        //     for (int k=0;k<n;k++) {
-        //         int[] col = new int[n];
-
-        //         for(int j = 0; j<n;j++) {
-        //             col[j] = grid[j][k];
-        //         }
-                
-        //         if(Arrays.equals(col, row)) {
-        //             count++;
-        //         }
-        //     }
-            
-
-           
-        // }
 
         Map<String, Integer> map = new HashMap<>();
 
