295020e7e8b6ff1abd05c6537757fcd7bd121efb
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-13 16:04:46 -0400

    Time: 1 ms (93.12%) | Memory: 45.1 MB (97.80%) - LeetSync

--- Stack/724-find-pivot-index/find-pivot-index.java
+++ Stack/724-find-pivot-index/find-pivot-index.java
@@ -9,12 +9,8 @@ class Solution {
         for(int i=1;i<nums.length;i++) {
             leftSum[i] = nums[i-1] + leftSum[i-1];
         }
-        System.out.println(Arrays.toString(leftSum));
 
         for(int i=0;i<nums.length;i++) {
-            System.out.println("leftSum:" + leftSum[i]);
-            System.out.println(totalSum - nums[i]);
-
             if(totalSum - nums[i] - leftSum[i] == leftSum[i] ) {
                 return i;
             }
