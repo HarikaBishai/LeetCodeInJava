986a9f2ff11bb7bc6eb664c0c9fda1ef0ef8e570
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-12 20:12:29 -0400

    Time: 1 ms (97.73%) | Memory: 45.9 MB (33.41%) - LeetSync

--- Array/605-can-place-flowers/can-place-flowers.java
+++ Array/605-can-place-flowers/can-place-flowers.java
@@ -1,18 +1,33 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        // if(n==0)return true;
+        // List<Integer> extended = new ArrayList<>();
+        // extended.add(0);
+
+        // for(int flower: flowerbed) {
+        //     extended.add(flower);
+        // }
+        // extended.add(0);
+
+        // for(int i=1; i<extended.size()-1; i++) {
+
+        //     if (extended.get(i-1) == 0  && extended.get(i) == 0 && extended.get(i+1) == 0) {
+        //         extended.set(i, 1);
+        //         n-=1;
+        //     }
+        //     if(n==0) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
         if(n==0)return true;
-        List<Integer> extended = new ArrayList<>();
-        extended.add(0);
+        int fn = flowerbed.length;
 
-        for(int flower: flowerbed) {
-            extended.add(flower);
-        }
-        extended.add(0);
+        for(int i=0; i<fn; i++) {
 
-        for(int i=1; i<extended.size()-1; i++) {
-
-            if (extended.get(i-1) == 0  && extended.get(i) == 0 && extended.get(i+1) == 0) {
-                extended.set(i, 1);
+            if ((i == 0 || flowerbed[i-1] == 0) && flowerbed[i] == 0 && (i==fn-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
                 n-=1;
             }
             if(n==0) {
@@ -20,5 +35,6 @@ class Solution {
             }
         }
         return false;
+
     }
 }
