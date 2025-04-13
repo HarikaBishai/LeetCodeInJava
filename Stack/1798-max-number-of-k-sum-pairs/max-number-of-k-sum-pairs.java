7ea06c722f2a40c51837ab26287b45adc0c43196
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-13 13:47:10 -0400

    Time: 42 ms (10.77%) | Memory: 59.4 MB (7.76%) - LeetSync

--- Stack/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ Stack/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -3,9 +3,12 @@ class Solution {
         Map <Integer, Integer> map = new HashMap<>();
         int count = 0;
         for(int num: nums) {
-            if(map.containsKey(k-num) && map.get(k-num)>0) {
+            if(map.containsKey((k-num))) {
                 count++;
                 map.put(k-num, map.get(k-num)-1);
+                if(map.get(k-num) == 0) {
+                    map.remove(k-num);
+                }
             } else {
                 map.put(num, map.getOrDefault(num, 0) + 1);
             }
