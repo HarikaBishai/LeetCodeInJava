6a09bb1bf66996e8042482b2e8c3ab1f08d8d1c1
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-13 16:46:44 -0400

    Time: 10 ms (77.49%) | Memory: 45.2 MB (95.75%) - LeetSync

--- Stack/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ Stack/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,19 +1,51 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        // List<List<Integer>> out = new ArrayList<>();
+
+        // Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        // Set<Integer> set2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+
+        // Set<Integer> diff1 = new HashSet<>(set1);
+        // diff1.removeAll(set2);
+
+        // Set<Integer> diff2 = new HashSet<>(set2);
+        // diff2.removeAll(set1);
+
+        // out.add(new ArrayList(diff1));
+        // out.add(new ArrayList(diff2));
+
+        // return out; 
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int num: nums1) set1.add(num);
+        for (int num: nums2) set2.add(num);
+
+        for(int num: nums2) {
+            if (set1.contains(num)) {
+                set1.remove(num);
+            }
+        }
+
+        for(int num: nums1) {
+            if (set2.contains(num)) {
+                set2.remove(num);
+            }
+        }
+
         List<List<Integer>> out = new ArrayList<>();
-
-        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
-        Set<Integer> set2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
-
-        Set<Integer> diff1 = new HashSet<>(set1);
-        diff1.removeAll(set2);
-
-        Set<Integer> diff2 = new HashSet<>(set2);
-        diff2.removeAll(set1);
-        
-        out.add(new ArrayList(diff1));
-        out.add(new ArrayList(diff2));
-
+        out.add(new ArrayList<>(set1));
+        out.add(new ArrayList<>(set2));
         return out;
+
+
+
+
+
+
+
+
+
     }
 }
