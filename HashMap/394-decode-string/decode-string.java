588eb504faeadee2999f231132230498ed5ee371
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-13 20:30:35 -0400

    Time: 7 ms (5.81%) | Memory: 41.8 MB (29.21%) - LeetSync

---
+++ HashMap/394-decode-string/decode-string.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public static boolean isInteger(String s) {
+        if(s == null || s.isEmpty()) return false;
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+    public String decodeString(String s) {
+        Stack<String> stk = new Stack<>();
+
+        for(char c: s.toCharArray()) {
+            if(c!=']') {
+                stk.push(String.valueOf(c));
+                System.out.println(stk);
+            } else {
+                StringBuilder currStr = new StringBuilder();
+
+                while(!stk.isEmpty() && !stk.peek().equals("[")) {
+                    currStr.append(stk.pop());
+                }
+                if(!stk.isEmpty()){
+                    stk.pop();
+                }
+
+                StringBuilder mutilplier = new StringBuilder();
+
+                while(!stk.isEmpty() && isInteger(stk.peek())) {
+                    mutilplier.append(stk.pop());
+                }
+
+                int value = 1;
+
+                
+                if(mutilplier.length() > 0) {
+                    mutilplier.reverse();
+                    value = Integer.parseInt(mutilplier.toString());
+                }
+
+                // currStr.reverse();
+                stk.push(currStr.toString().repeat(value));
+
+
+            }
+        }
+        Collections.reverse(stk);
+        String result =  stk.stream().collect(Collectors.joining());
+        return new StringBuilder(result).reverse().toString();
+       
+    }
+}
