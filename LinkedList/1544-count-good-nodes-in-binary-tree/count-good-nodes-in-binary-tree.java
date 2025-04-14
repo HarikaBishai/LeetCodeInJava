5eb51531f25af397061d62bac4c0a66baeffa17e
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-14 01:38:16 -0400

    Time: 2 ms (85.56%) | Memory: 52.2 MB (75.54%) - LeetSync

--- LinkedList/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ LinkedList/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -16,11 +16,11 @@
 class Solution {
     public int goodNodes(TreeNode root) {
         
-        return getgoodNode(root, root.val);
+        return getgoodNodes(root, root.val);
         
     }
 
-    public static int getgoodNode(TreeNode root,int maxValue){
+    public static int getgoodNodes(TreeNode root,int maxValue){
         if (root == null) return 0;
         int count = 0;
 
@@ -28,7 +28,7 @@ class Solution {
             count++;
             maxValue = root.val;
         }
-        return count + getgoodNode(root.left, maxValue) + getgoodNode(root.right, maxValue);
+        return count + getgoodNodes(root.left, maxValue) + getgoodNodes(root.right, maxValue);
 
     }
 }
