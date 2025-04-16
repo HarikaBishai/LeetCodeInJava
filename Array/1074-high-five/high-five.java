6ef7445b47c369e7f973bc7fa5b2628ab3a858f7
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-16 01:14:54 -0400

    Time: 5 ms (74.06%) | Memory: 45 MB (38.71%) - LeetSync

--- Array/1074-high-five/high-five.java
+++ Array/1074-high-five/high-five.java
@@ -1,29 +1,35 @@
 class Solution {
     public int[][] highFive(int[][] items) {
 
-        Arrays.sort(items, (a,b) -> b[1]-a[1]);
 
-        Map <Integer, List<Integer>> map = new TreeMap<>();
+        Map <Integer, PriorityQueue<Integer>> map = new TreeMap<>();
 
         for(int[] item: items) {
             int id = item[0];
             int score = item[1];
 
-            List<Integer> studentScores = map.getOrDefault(id, new ArrayList<>());
-            if(studentScores.size()<5) {
-                studentScores.add(score);
-                map.put(id, studentScores);
-            }
+            map.putIfAbsent(id, new PriorityQueue<>());
 
+            Queue<Integer> pq = map.get(id);
+            pq.offer(score);
+            if(pq.size()>5) {
+                pq.poll();
+            }
             
         }
 
         int[][] out = new int[map.size()][2];
         int i = 0;
         for(int key: map.keySet()) {
-            System.out.println(key);
+           
             out[i][0] = key;
-            out[i][1] = (map.get(key).stream().mapToInt(Integer::intValue).sum())/(map.get(key).size());
+            Queue<Integer> pq = map.get(key);
+            while(!pq.isEmpty()) {
+                out[i][1]+=pq.poll();
+            }
+            
+            out[i][1] /= 5;
+
             i++;
         }
 
