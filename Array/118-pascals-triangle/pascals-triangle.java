290c8bc375945d0a5d8d30846ef934fe0b322ee8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-16 06:25:45 -0400

    Time: 1 ms (85.00%) | Memory: 41.9 MB (78.93%) - LeetSync

--- Array/118-pascals-triangle/pascals-triangle.java
+++ Array/118-pascals-triangle/pascals-triangle.java
@@ -14,8 +14,7 @@ class Solution {
                 } else if(i==row) {
                     rowArray.add(1);
                 } else {
-                    if(row-1>=0 && i-1>=0)
-                        rowArray.add(out.get(row-1).get(i-1) + out.get(row-1).get(i) );
+                    rowArray.add(out.get(row-1).get(i-1) + out.get(row-1).get(i) );
                 }
             }
             out.add(rowArray);
