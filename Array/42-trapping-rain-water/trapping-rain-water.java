11f7eca62324c3cdbecfb383adbee813405bdf31
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-30 19:55:46 -0400

    Time: 6 ms (6.79%) | Memory: 46 MB (88.54%) - LeetSync

--- Array/42-trapping-rain-water/trapping-rain-water.java
+++ Array/42-trapping-rain-water/trapping-rain-water.java
@@ -1,24 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        
-
         Stack<Integer> stk = new Stack<>();
-        int totalWater = 0;
-
-        for(int i=0; i< height.length; i++) {
+        int water = 0;
+        for(int i=0;i<height.length;i++) {
             while(!stk.isEmpty() && height[stk.peek()] < height[i]) {
                 int curr = stk.pop();
+
                 if(!stk.isEmpty()) {
                     int leftIndex = stk.peek();
                     int leftHeight = height[leftIndex];
-                    int rightHeight =height[i];
-                    totalWater += (i-leftIndex-1) * (Math.min(leftHeight,rightHeight) - height[curr]);
+                    int rightHeight = height[i];
 
+                    int depth = Math.min(leftHeight, rightHeight) - height[curr];
+                    int breadth = i-leftIndex-1;
+
+                    water += depth * breadth;
                 }
             }
             stk.push(i);
         }
-
-        return totalWater;
+        return water;
     }
 }
