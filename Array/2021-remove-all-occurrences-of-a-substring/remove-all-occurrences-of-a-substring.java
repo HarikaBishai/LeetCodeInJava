12e4302a5ac5de084b37e177b0c0907d6ea7448d
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-05-01 04:14:59 -0400

    Time: 9 ms (24.10%) | Memory: 43.8 MB (54.97%) - LeetSync

--- Array/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ Array/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -17,22 +17,40 @@ class Solution {
 
     public String removeOccurrences(String s, String part) {
 
-        stk = new Stack<>();
+        // stk = new Stack<>();
+
+        Deque<Character> dq = new ArrayDeque<>();
 
         StringBuilder sb= new StringBuilder();
+        int partLen = part.length();
 
         for(char c: s.toCharArray()) {
-            stk.push(c);
+            dq.push(c);
 
-            while(stk.size()>=part.length() && checkEqual(part)) {
-                for(int i = 0; i<part.length();i++) {
-                    stk.pop();
+            while(dq.size()>=partLen) {
+                StringBuilder temp= new StringBuilder();
+                Iterator<Character> it = dq.iterator();
+                for(int i = partLen-1; i>=0;i--) {
+                    char curr = it.next();
+                    if(curr!= part.charAt(i)) {
+                        break;
+                    }
+                    temp.append(curr);
+                }
+
+                if(temp.length() < partLen) {
+                    break;
+                }
+                int i=0;
+                while(i< partLen) {
+                    dq.pop();
+                    i++;
                 }
             }
         }
 
-        while(!stk.isEmpty()) {
-            sb.append(stk.pop());
+        while(!dq.isEmpty()) {
+            sb.append(dq.pop());
         }
 
         sb.reverse();
