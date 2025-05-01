a5604dc570c34b03a121f68a414202c23262627c
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-04-30 23:53:12 -0400

    Time: 6 ms (98.07%) | Memory: 47.7 MB (91.14%) - LeetSync

--- Array/49-group-anagrams/group-anagrams.java
+++ Array/49-group-anagrams/group-anagrams.java
@@ -1,7 +1,5 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> out = new ArrayList<>();
-
         Map<String, List<String>> map = new HashMap<>();
 
         for(String s: strs) {
@@ -13,9 +11,6 @@ class Solution {
             group.add(s);
         }
 
-        // for(List<String> group: map.values()) {
-        //     out.add(group);
-        // }
         return new ArrayList<>(map.values());
 
 
