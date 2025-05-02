f62e726a90c869b85987dd9f27749f3512f1c4b8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-05-02 04:36:55 -0400

    Time: 16 ms (7.14%) | Memory: 45.5 MB (92.45%) - LeetSync

---
+++ Array/210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, Integer> indegree = new HashMap<>();
+
+        for(int i=0;i<numCourses;i++) {
+            indegree.put(i, 0);
+        }
+
+
+        for(int[] prereq: prerequisites) {
+            int v = prereq[0];
+            int u = prereq[1];
+
+            graph.putIfAbsent(u, new ArrayList<>());
+            List<Integer> nodes = graph.get(u);
+
+            nodes.add(v);
+            indegree.put(v, indegree.get(v)+1);
+        }
+
+        Queue<Integer>q = new LinkedList<>();
+
+        for(int key: indegree.keySet()) {
+            if(indegree.get(key) == 0) {
+                q.offer(key);
+            }
+        }
+
+
+
+        List<Integer> out = new ArrayList<>();
+
+        while(!q.isEmpty()) {
+            int node = q.poll();
+
+            out.add(node);
+
+            List<Integer> neighbours = graph.getOrDefault(node, new ArrayList<>());
+
+            for(int nei: neighbours) {
+                indegree.put(nei, indegree.get(nei)-1);
+                if(indegree.get(nei) == 0) {
+                    q.offer(nei);
+                }
+            }
+            
+        }
+
+        System.out.println(out);
+
+        return (out.size() != numCourses) ? new int[]{} 
+            : out.stream().mapToInt(Integer::intValue).toArray();
+
+
+
+
+    }
+}
