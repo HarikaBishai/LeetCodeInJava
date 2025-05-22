fbfb8f704279cb9cc6af7e8aa1037e8233a29fd0
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-05-22 15:34:41 -0400

    Time: 41 ms (5.02%) | Memory: 57.7 MB (5.09%) - LeetSync

--- Array/200-number-of-islands/number-of-islands.java
+++ Array/200-number-of-islands/number-of-islands.java
@@ -5,38 +5,39 @@ class Solution {
     private Set<String> visited;
     private char[][] grid;
 
-    public  void dfs(int r, int c) {
-        visited.add(r+"_"+c);
-        int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    public void dfs(int r, int c) {
+        visited.add(r+"-"+c);
+        int[][] dirs = {{0,-1}, {0, 1}, {-1,0}, {1,0}};
 
-        for(int[] dir : dirs ) {
-            int new_r = dir[0] + r;
-            int new_c = dir[1] + c;
+        for(int[] dir: dirs) {
+            int new_r = r + dir[0];
+            int new_c = c + dir[1];
 
-            if(0<=new_r && new_r< ROWS && 0<=new_c && new_c< COLS && !visited.contains(new_r+"_" + new_c) && grid[new_r][new_c] == '1' ) {
+            if(new_r>=0 && new_r<ROWS && new_c>=0 && new_c<COLS && grid[new_r][new_c] == '1' && !visited.contains(new_r + "-" + new_c)) {
                 dfs(new_r, new_c);
             }
+
         }
     }
 
     public int numIslands(char[][] grid) {
+        ROWS = grid.length;
+        COLS = grid[0].length;
+        visited = new HashSet<>();
         this.grid = grid;
-        this.ROWS = grid.length;
-        this.COLS = grid[0].length;
-        this.visited = new HashSet<>();
-       
-        int islands = 0;
-        for(int i=0;i<ROWS;i++) {
-            for(int j = 0; j<COLS; j++) {
-                if(grid[i][j] == '1' && !visited.contains(i+"_"+ j)) {
-                    islands++;
+        int islandsCount = 0;
+
+        for(int i=0;i<ROWS; i++) {
+            for(int j=0;j<COLS;j++) {
+                if(grid[i][j] == '1' && !visited.contains(i+"-" + j)) {
                     dfs(i,j);
+                    islandsCount++;
                 }
             }
         }
-        return islands;
-
+        return islandsCount;
         
+
 
     }
 }
