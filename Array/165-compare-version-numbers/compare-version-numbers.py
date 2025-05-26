be7d30fae2a2c073b4eadd605c9194a308797211
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-05-26 16:35:20 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.51%) - LeetSync

--- Array/165-compare-version-numbers/compare-version-numbers.py
+++ Array/165-compare-version-numbers/compare-version-numbers.py
@@ -3,32 +3,21 @@ class Solution:
         
         version1 = version1.split(".")
         version2 = version2.split(".")
+        i=0
+        while i<len(version1) or i<len(version2):
+            part1 = version1[i] if i < len(version1) else "0"
+            part2 = version2[i] if i < len(version2) else "0"
 
-
-
-        def compare(part1, part2):
             part1.lstrip("0")
             part2.lstrip("0")
-        
+            part1 = int(part1)
+            part2 = int(part2)
 
 
             if int(part1) > int(part2):
                 return 1
             elif int(part2) > int(part1):
                 return -1
-            else:
-                return 0
-
-        i = 0
-
-        
-        while i<len(version1) or i<len(version2):
-            part1 = version1[i] if i < len(version1) else "0"
-            part2 = version2[i] if i < len(version2) else "0"
-
-            val = compare(part1, part2)
-            if val!= 0:
-                return val
             
             i+=1
         return 0
