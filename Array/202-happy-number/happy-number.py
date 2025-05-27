117261587ad6247f70d0c6869915f70d13c0cb79
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-05-27 17:22:53 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.06%) - LeetSync

---
+++ Array/202-happy-number/happy-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        seen = set()
+
+        while n not in seen:
+
+            temp = n
+            seen.add(n)
+            sum = 0
+            while temp > 0:
+                sum += pow(temp%10, 2) 
+                temp = temp//10
+            
+            if sum == 1:
+                return True
+            n = sum
+            
+            
+        return False
