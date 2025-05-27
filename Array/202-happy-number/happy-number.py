5a3a300d9df4a3170b6aa7d14da16ea1f582f463
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-05-27 17:23:00 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.32%) - LeetSync

--- Array/202-happy-number/happy-number.py
+++ Array/202-happy-number/happy-number.py
@@ -4,14 +4,12 @@ class Solution:
         seen = set()
 
         while n not in seen:
-
             temp = n
             seen.add(n)
             sum = 0
             while temp > 0:
                 sum += pow(temp%10, 2) 
                 temp = temp//10
-            
             if sum == 1:
                 return True
             n = sum
