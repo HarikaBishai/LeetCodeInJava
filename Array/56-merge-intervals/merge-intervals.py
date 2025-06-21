e5221cc5587fe7be25e8fe1cd4eef2f032b88453
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-06-21 02:31:27 -0400

    Time: 13 ms (13.00%) | Memory: 21.8 MB (15.01%) - LeetSync

--- Array/56-merge-intervals/merge-intervals.py
+++ Array/56-merge-intervals/merge-intervals.py
@@ -1,14 +1,12 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         stk = []
-
         intervals.sort()
 
-        for start, end in intervals:
-            if stk and stk[-1][1] >= start:
-                last = stk.pop()
-                stk.append([last[0], max(last[1], end)])
+        for st, end in intervals:
+            if stk and stk[-1][1] >= st:
+                top = stk.pop()
+                stk.append([top[0], max(end, top[1])])
             else:
-                stk.append([start, end])
-        
+                stk.append([st, end])
         return stk
