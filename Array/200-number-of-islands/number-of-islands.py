f2449d2bd0ac4d8a64a07b90fdf5224fedd1d34d
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-06-21 02:09:00 -0400

    Time: 324 ms (8.21%) | Memory: 28.3 MB (11.25%) - LeetSync

--- Array/200-number-of-islands/number-of-islands.py
+++ Array/200-number-of-islands/number-of-islands.py
@@ -3,25 +3,24 @@ class Solution:
         ROWS = len(grid)
         COLS = len(grid[0])
 
+        islands = 0
         visited = set()
-
-
         def dfs(r, c):
             visited.add((r,c))
-            
-            dirs = [(-1,0),(1,0),(0,-1),(0,1)]
 
-            for i, j in dirs:
-                new_r = r+i
-                new_c = c+j
-                if new_r in range(ROWS) and new_c in range(COLS) and grid[new_r][new_c]=='1' and (new_r, new_c) not in visited:
+            dir = [(-1, 0), (1,0), (0,-1), (0,1)]
+
+            for i, j in dir:
+                new_r = i + r
+                new_c = j + c
+                if new_r in range(ROWS) and new_c in range(COLS) and grid[new_r][new_c] == "1" and (new_r, new_c) not in visited:
                     dfs(new_r, new_c)
-        
 
-        islands = 0
+        
         for i in range(ROWS):
             for j in range(COLS):
-                if grid[i][j] == '1' and  (i,j) not in visited:
-                    dfs(i,j)
+                if grid[i][j] == "1" and (i,j) not in visited:
                     islands+=1
-        return islands 
+                    dfs(i,j)
+
+        return islands
