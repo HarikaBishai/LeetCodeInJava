1dffbe0c4c11f30dc81d263f7df16b8c1a10c44f
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-06-22 20:30:16 -0400

    Time: 78 ms (57.88%) | Memory: 19.2 MB (61.75%) - LeetSync

--- Array/621-task-scheduler/task-scheduler.py
+++ Array/621-task-scheduler/task-scheduler.py
@@ -1,13 +1,13 @@
 import heapq
-from collections import deque
+from collections import Counter, deque
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        maxHeap = []
-
+        
         counter = Counter(tasks)
-
+        maxHeap = []
         for key, count in counter.items():
             heapq.heappush(maxHeap, -count)
+        
 
         q = deque()
 
@@ -15,14 +15,14 @@ class Solution:
 
         while maxHeap or q:
             time+=1
+
             if not maxHeap:
                 time = q[0][1]
             else:
-                cnt = heapq.heappop(maxHeap)+1
+                cnt = heapq.heappop(maxHeap) + 1
                 if cnt:
                     q.append((cnt, time+n))
             
-            if q and  time == q[0][1]:
-                cnt = q.popleft()[0]
-                heapq.heappush(maxHeap, cnt)
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
         return time
