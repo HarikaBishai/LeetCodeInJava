1bb2069d45e6bc11cae66c29427176f1d87b6fa1
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-06-23 02:18:12 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (19.12%) - LeetSync

---
+++ Array/362-design-hit-counter/design-hit-counter.py
@@ -0,0 +1,29 @@
+from collections import deque
+class HitCounter:
+
+    def __init__(self):
+        self.hits = deque()
+        self.total=0
+        
+
+    def hit(self, timestamp: int) -> None:
+        if not self.hits or self.hits[-1][0] < timestamp:
+            self.hits.append([timestamp, 1]) 
+        else:
+            self.hits[-1][1] += 1
+        self.total+=1
+
+    def getHits(self, timestamp: int) -> int:
+        # while self.hits and self.hits[0][0] < timestamp-300:
+        #     self.hits.popleft()
+        
+        while self.hits and self.hits[0][0] <= timestamp -300:
+            t, cnt = self.hits.popleft()
+            self.total-=cnt
+        return self.total
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
