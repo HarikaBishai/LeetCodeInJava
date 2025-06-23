3e0a610dc9b1e72c08c96ebc5dcdd5501f97a9d1
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-06-23 01:54:50 -0400

    Time: 650 ms (39.98%) | Memory: 40 MB (51.92%) - LeetSync

--- Array/1710-find-servers-that-handled-most-number-of-requests/find-servers-that-handled-most-number-of-requests.py
+++ Array/1710-find-servers-that-handled-most-number-of-requests/find-servers-that-handled-most-number-of-requests.py
@@ -1,25 +1,22 @@
 from sortedcontainers import SortedList
-import heapq
-from typing import List
 class Solution:
     def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
-        
-        count = [0] * k
-
         busy, free = [], SortedList(list(range(k)))
-
+        count =[0] * k
 
         for i in range(len(arrival)):
             start = arrival[i]
-            while busy and busy[0][0] <= start:
-                time, busy_id = heapq.heappop(busy)
-                free.add(busy_id)
+            while busy and busy[0][0]<=start:
+                time, server_id = heapq.heappop(busy)
+                free.add(server_id)
+            
             if free:
                 idx = free.bisect_left(i%k)
                 server_id = free[idx] if idx < len(free) else free[0]
                 count[server_id]+=1
                 free.remove(server_id)
-                heapq.heappush(busy, (start+load[i], server_id))
+                heapq.heappush(busy, (start+load[i],server_id ))
+
         max_count = max(count)
 
-        return [idx for idx, cnt in enumerate(count) if cnt == max_count ]
+        return [idx for idx, val in enumerate(count) if val == max_count]
