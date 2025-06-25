684f7161f2fed8a6fc61441bf1e639e688aab623
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-06-25 17:09:31 -0400

    Time: 11 ms (83.39%) | Memory: 20.7 MB (76.77%) - LeetSync

--- Array/49-group-anagrams/group-anagrams.py
+++ Array/49-group-anagrams/group-anagrams.py
@@ -4,12 +4,8 @@ class Solution:
         groups = defaultdict(list)
 
         for s in strs:
-            char_list = list(s)
-            char_list.sort()
-            sorted_s = "".join(char_list)
+            sorted_s = ''.join(sorted(s))
             groups[sorted_s].append(s)
 
-            # groups["".join(sorted(s))].append(s)
-        
         return list(groups.values())
 
