1d056c7a35f307094ce67c2b122552d962f21f1c
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-10-20 20:00:06 -0400

    Time: 34 ms (80.57%) | Memory: 17.9 MB (8.53%) - LeetSync

--- Array/1894-merge-strings-alternately/merge-strings-alternately.py
+++ Array/1894-merge-strings-alternately/merge-strings-alternately.py
@@ -13,17 +13,5 @@ class Solution:
         for i in range(idx+1, len(word2)):
             merged += word2[i]
 
-        # while l1 < n1 or l2 < n2:
-        #     if l1 < n1 and l2 < n2:
-        #         merged += word1[l1]
-        #         merged += word2[l2]
-        #         l1+=1
-        #         l2+=1
-        #     elif l1 < n1:
-        #         merged += word1[l1]
-        #         l1+=1
-        #     else:
-        #         merged+= word2[l2]
-        #         l2+=1
         return merged
 
