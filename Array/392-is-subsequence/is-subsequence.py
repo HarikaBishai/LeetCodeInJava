355735a41803df4d10f5912deb14e56e51178e58
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2025-10-22 19:34:39 -0400

    Time: 3 ms (25.76%) | Memory: 17.8 MB (67.53%) - LeetSync

--- Array/392-is-subsequence/is-subsequence.py
+++ Array/392-is-subsequence/is-subsequence.py
@@ -1,7 +1,6 @@
 from collections import Counter
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if s == t: return True
         s_left = 0
         t_left = 0
         while s_left < len(s) and t_left < len(t):
