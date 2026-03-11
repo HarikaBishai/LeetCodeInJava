f23bdb6e52dda4c3bd0d554d29f1ac59f71bc747
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-11 15:20:51 -0400

    Time: 84 ms (7.15%) | Memory: 29.5 MB (5.07%) - LeetSync

---
+++ JavaScript/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        maxPrice = prices[-1]
+
+        for i in range(len(prices)-2, -1, -1):
+            maxPrice = max(maxPrice, prices[i])
+            maxProfit = max(maxProfit, maxPrice-prices[i])
+        
+        return maxProfit
+            
