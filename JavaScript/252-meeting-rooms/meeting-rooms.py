f4a67c29b12f27cd460cd0ff9663d9078ff5a854
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-11 17:03:04 -0400

    Time: 3 ms (72.66%) | Memory: 21.6 MB (6.02%) - LeetSync

--- JavaScript/252-meeting-rooms/meeting-rooms.py
+++ JavaScript/252-meeting-rooms/meeting-rooms.py
@@ -1,11 +1,12 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
+
         stk = []
 
         for start, end in intervals:
-               
-            if stk and start < stk[-1][1]:
-                    return False
+            if stk and stk[-1][1] > start:
+                return False
             stk.append([start, end])
+        
         return True
