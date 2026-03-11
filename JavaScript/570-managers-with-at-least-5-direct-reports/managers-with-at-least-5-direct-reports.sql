224a30e666749f61832cd0e13efa71332ee1ffe9
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-11 01:06:17 -0400

    Time: 2299 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- JavaScript/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ JavaScript/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT e1.name FROM Employee e1
-WHERE (
+WHERE e1.id IN (
     SELECT e2.managerId FROM Employee e2
     WHERE e1.id = e2.managerId
     GROUP BY e2.managerId
