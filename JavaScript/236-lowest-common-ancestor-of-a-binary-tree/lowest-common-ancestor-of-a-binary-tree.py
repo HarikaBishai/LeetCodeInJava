71e2b17c320efbb1a42034111390b71dd6049b92
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-28 00:36:16 -0400

    Time: 51 ms (93.04%) | Memory: 24.5 MB (41.25%) - LeetSync

--- JavaScript/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ JavaScript/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,24 +7,12 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        parentObj = {root: None}
-
-        def getParentObj(root):
-            if not root:
-                return 
-            if root.left:
-                parentObj[root.left] = root
-            if root.right:
-                parentObj[root.right] = root
-            getParentObj(root.left)
-            getParentObj(root.right)
-
-        getParentObj(root)
-        pathSetP = set() 
-        while p:
-            pathSetP.add(p)
-            p = parentObj[p]
-
-        while q and q not in pathSetP:
-            q =  parentObj[q]
-        return q
+        if not root or root==p or root==q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        
+        return left if left else right
