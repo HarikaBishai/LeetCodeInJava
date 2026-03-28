a08ffafe2aaab9d902fb8d1ee4791e0094b58c6e
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-28 17:08:22 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (55.70%) - LeetSync

---
+++ JavaScript/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        out = 0
+        path = []
+
+        def dfs(root):
+            nonlocal out
+            if not root:
+                return 0
+            
+            path.append(str(root.val))
+
+            if not root.left and not root.right:
+                out+= int(''.join(path))
+            
+            dfs(root.left)
+            dfs(root.right)
+            path.pop()
+        dfs(root)
+        return out
+            
+
+
