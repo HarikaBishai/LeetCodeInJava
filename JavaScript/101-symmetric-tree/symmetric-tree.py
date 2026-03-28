594b814a4fc63a74cd98777546e547af087ead46
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-28 01:25:25 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.81%) - LeetSync

---
+++ JavaScript/101-symmetric-tree/symmetric-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        def isSimilar(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val!=q.val:
+                return False
+            return isSimilar(p.left, q.right) and isSimilar(p.right, q.left)
+        
+        if not root:
+            return True
+        return isSimilar(root.left, root.right)
+            
+        
