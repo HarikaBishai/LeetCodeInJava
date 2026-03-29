30d963fb21ce571bfea96c03eca8dfd545daef48
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-29 12:52:02 -0400

    Time: 4 ms (55.71%) | Memory: 21 MB (83.02%) - LeetSync

---
+++ JavaScript/106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+
+        inorder_map = {val:i for i,val in enumerate(inorder)}
+        postorder_index = len(postorder) - 1
+        
+        def dfs(left, right):
+            nonlocal postorder_index
+            if left > right:
+                return None
+            if(postorder_index < 0): return None
+            root_val = postorder[postorder_index]
+            mid = inorder_map[root_val]
+
+            root = TreeNode(root_val)
+            postorder_index-=1
+            root.right = dfs(mid+1, right)
+            root.left = dfs(left, mid-1)
+            
+
+            return root
+        return dfs(0, len(inorder)-1)
