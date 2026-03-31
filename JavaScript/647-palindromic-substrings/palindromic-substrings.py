7fc156116ff498ba6a4216cf2f20a08f788e1c2f
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-30 23:43:55 -0400

    Time: 107 ms (89.73%) | Memory: 19.4 MB (32.56%) - LeetSync

--- JavaScript/647-palindromic-substrings/palindromic-substrings.py
+++ JavaScript/647-palindromic-substrings/palindromic-substrings.py
@@ -3,20 +3,32 @@ class Solution:
         if len(s) <=1:
             return 1
         n = len(s)
-
-        dp = [[False]*n for _ in range(n)]
-
-
-        for i in range(n):
-            dp[i][i] = True
-        
         count = 0
-
-        for length in range(1, n+1):
-            for i in range(n-length+1):
-                j = i + length - 1
-                if s[i] == s[j] and (length <= 2 or  dp[i+1][j-1]):
-                    dp[i][j] = True
-                    count+=1
+        for i in range(n):
+            l = i
+            r = i
+            while l >= 0 and r < n and s[l] == s[r]:
+                l-=1
+                r+=1
+                count+=1
+            l = i
+            r = i+1
+            while  l >= 0 and r < n and s[l] == s[r]:
+                l-=1
+                r+=1
+                count+=1
         return count
+
+        # for i in range(n):
+        #     dp[i][i] = True
+        
+        # count = 0
+
+        # for length in range(1, n+1):
+        #     for i in range(n-length+1):
+        #         j = i + length - 1
+        #         if s[i] == s[j] and (length <= 2 or  dp[i+1][j-1]):
+        #             dp[i][j] = True
+        #             count+=1
+        # return count
                     
