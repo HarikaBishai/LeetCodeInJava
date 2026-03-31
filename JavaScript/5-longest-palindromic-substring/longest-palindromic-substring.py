2600a1b23a4cafb89100ce8d07200b8374242cf7
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-30 21:30:30 -0400

    Time: 251 ms (66.25%) | Memory: 19.2 MB (95.48%) - LeetSync

--- JavaScript/5-longest-palindromic-substring/longest-palindromic-substring.py
+++ JavaScript/5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,49 +1,47 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
-            return s
-        n = len(s)
-        dp = [[False]*n for _ in range(n)]
+        # if len(s) <= 1:
+        #     return s
+        # n = len(s)
+        # dp = [[False]*n for _ in range(n)]
 
-        for i in range(n):
-            dp[i][i] = True
+        # for i in range(n):
+        #     dp[i][i] = True
         
-        maxLength = 1
+        # maxLength = 1
+        # start = 0
+
+        # for length in range(2, n+1):
+        #     for i in range(n-length+1):
+        #         j = i+length-1
+        #         if s[i] == s[j] and (length == 2 or dp[i+1][j-1]):
+        #             dp[i][j]=True
+        #             if length > maxLength:
+        #                 maxLength = length
+        #                 start = i
+        
+        # return s[start: start+maxLength]
+        maxLen = 0
         start = 0
+        n = len(s)
+        for i in range(n):
+            l = i
+            r = i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r-l+1 > maxLen:
+                    maxLen = r-l+1
+                    start = l
+                l -=1
+                r+=1
+            
+            l = i
+            r = i+1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r-l+1 > maxLen:
+                    maxLen = r-l+1
+                    start = l
+                l -=1
+                r+=1
+        return s[start: start+maxLen]
 
-        for length in range(2, n+1):
-            for i in range(n-length+1):
-                j = i+length-1
-                if s[i] == s[j] and (length == 2 or dp[i+1][j-1]):
-                    dp[i][j]=True
-                    if length > maxLength:
-                        maxLength = length
-                        start = i
         
-        return s[start: start+maxLength]
-
-
-        # maxLen = 0
-
-        # index = 0
-        # for i in range(len(s)):
-        #     l = i
-        #     r = i
-
-        #     while l>=0 and r<len(s) and s[l] == s[r]:
-        #         if r-l+1 > maxLen:
-        #             maxLen = r-l+1
-        #             index = l
-        #         r+=1
-        #         l-=1
-            
-        #     l=i
-        #     r = i+1
-            
-        #     while l>=0 and r<len(s) and s[l] == s[r]:
-        #         if r-l+1 > maxLen:
-        #             maxLen = r-l+1
-        #             index = l
-        #         r+=1
-        #         l-=1
-        # return s[index: index+maxLen]
