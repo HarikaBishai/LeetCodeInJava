3660a35d72e1c549e16c12e253964075305029ec
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-31 02:20:11 -0400

    Time: 1247 ms (51.28%) | Memory: 19.4 MB (73.11%) - LeetSync

--- JavaScript/300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ JavaScript/300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         dp = [1] * n
 
-        for i in range(n): 
+        for i in range(1,n): 
             for j in range(i):
                 if nums[j] < nums[i]:
                     dp[i] = max(dp[i], 1+dp[j])
