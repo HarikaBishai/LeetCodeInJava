f9a07d3046e13c909ff1a01823c7b834b48ed2db
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-31 00:59:24 -0400

    Time: 436 ms (92.47%) | Memory: 19.7 MB (47.05%) - LeetSync

--- JavaScript/322-coin-change/coin-change.py
+++ JavaScript/322-coin-change/coin-change.py
@@ -2,19 +2,27 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
 
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
 
-        dp = [[float('inf')]*(amount+1) for _ in range(n+1)]
+        for coin in coins:
+            for t in range(coin, amount+1):
+                dp[t] = min(dp[t], 1+dp[t-coin])
+        
+        return dp[amount] if dp[amount]!=float('inf') else -1
 
-        for i in range(n+1):
-            dp[i][0] = 0
+        # dp = [[float('inf')]*(amount+1) for _ in range(n+1)]
 
-        for i in range(1, n+1):
-            coin = coins[i-1]
+        # for i in range(n+1):
+        #     dp[i][0] = 0
 
-            for t in range(1, amount+1):
-                dp[i][t] = dp[i-1][t]
+        # for i in range(1, n+1):
+        #     coin = coins[i-1]
 
-                if t>=coin:
-                    dp[i][t] = min(dp[i][t], 1+dp[i][t-coin])
+        #     for t in range(1, amount+1):
+        #         dp[i][t] = dp[i-1][t]
+
+        #         if t>=coin:
+        #             dp[i][t] = min(dp[i][t], 1+dp[i][t-coin])
 
         return dp[n][amount] if dp[n][amount]!=float('inf') else -1
