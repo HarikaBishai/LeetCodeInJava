b1e2dde16c0fa2c097a3898a31d7380357420787
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-03-31 21:31:54 -0400

    Time: 19 ms (53.20%) | Memory: 19.6 MB (58.62%) - LeetSync

---
+++ JavaScript/40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        path = []
+        def dfs(start, path, total):
+            if total == target:
+                res.append(path[:])
+                return 
+            if total > target:
+                return 
+            
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i-1] == candidates[i]:
+                    continue
+                path.append(candidates[i])
+                dfs(i+1, path, total+candidates[i])
+                path.pop()
+        
+        dfs(0,[],0)
+        return res
