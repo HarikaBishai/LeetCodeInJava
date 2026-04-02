a217ed3c427e915c84874b9ed79e054d1699e59a
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-04-01 22:02:59 -0400

    Time: 10 ms (14.73%) | Memory: 20.4 MB (86.53%) - LeetSync

---
+++ JavaScript/207-course-schedule/course-schedule.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        graph = {i:[] for i in range(numCourses)}
+        indegree = {i: 0 for i in range(numCourses)}
+        for v, u in prerequisites:
+            graph[u].append(v)
+            indegree[v]+=1
+        
+        q = deque()
+        visited = set()
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+                
+
+        while q:
+            node = q.popleft()
+            visited.add(node)
+            
+            for nei in graph[node]:
+                indegree[nei]-=1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        return len(visited) == numCourses
