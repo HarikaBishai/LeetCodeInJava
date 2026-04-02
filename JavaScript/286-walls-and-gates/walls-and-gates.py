e79138ed63a3afa70b3bcec78bb97e79a5471527
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-04-02 01:51:58 -0400

    Time: 55 ms (29.13%) | Memory: 25.7 MB (26.70%) - LeetSync

---
+++ JavaScript/286-walls-and-gates/walls-and-gates.py
@@ -0,0 +1,32 @@
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        
+
+        m = len(rooms)
+        n = len(rooms[0])
+        INF = 2147483647
+        q = deque()
+        visited = set()
+        for i in range(m):
+            for j in range(n):
+                if rooms[i][j] == 0:
+                    visited.add((i,j))
+                    q.append((i,j))
+
+        dir = [(-1,0), (1,0), (0,1), (0,-1)]
+        level = 0 
+        while q:
+            level+=1
+            for _ in range(len(q)):
+                r,c = q.popleft()
+                for i, j in dir:
+                    new_r = r+i
+                    new_c = c+j
+                    if new_r in range(m) and new_c in range(n) and rooms[new_r][new_c] == INF:
+                        visited.add((new_r, new_c))
+                        rooms[new_r][new_c] = level
+                        q.append((new_r, new_c))
+        
