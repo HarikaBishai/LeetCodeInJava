c98e1cb3d45991546a052a8071691d8e735eccc1
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-04-03 02:59:21 -0400

    Time: 2540 ms (19.00%) | Memory: 81.2 MB (75.22%) - LeetSync

---
+++ JavaScript/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        
+        if len(points) == 1:
+            return 0
+        
+        n = len(points)
+        visited = set()
+        min_cost = 0
+
+        h = [(0, 0)]
+
+        while h:
+            cost , pt = heapq.heappop(h)
+
+            if pt in visited:
+                continue
+            
+            if len(visited) == n:
+                return min_cost
+            visited.add(pt)
+            min_cost+= cost
+
+            for i in range(n):
+                if i!=pt and i not in visited:
+                    x1,y1 = points[pt]
+                    x2,y2 = points[i]
+
+                    new_cost = abs(x1-x2) + abs(y1-y2)
+                    heapq.heappush(h, (new_cost, i))
+        return min_cost
