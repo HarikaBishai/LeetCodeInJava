dfd846bc696f4e10f9cd7c11134d67436031b3d8
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-05-02 21:45:44 -0400

    Time: 436 ms (31.52%) | Memory: 0B (100.00%) - LeetSync

--- JavaScript/1258-article-views-i/article-views-i.sql
+++ JavaScript/1258-article-views-i/article-views-i.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
-
-SELECT DISTINCT author_id as id FROM Views WHERE author_id = viewer_id ORDER BY author_id;
+SELECT author_id AS id 
+FROM Views
+WHERE author_id = viewer_id 
+GROUP BY author_id
+HAVING COUNT(*) >= 1
+ORDER BY author_id;
