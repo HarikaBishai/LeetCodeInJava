1b4659177b4c50688f6c9e574af2b26875bccdfc
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-05-11 02:58:20 -0400

    Time: 421 ms (76.20%) | Memory: 0B (100.00%) - LeetSync

--- JavaScript/197-rising-temperature/rising-temperature.sql
+++ JavaScript/197-rising-temperature/rising-temperature.sql
@@ -1,8 +1,21 @@
 # Write your MySQL query statement below
 
-SELECT w1.id
-FROM weather w1
-WHERE w1.temperature >
-(SELECT temperature 
-FROM weather w2
-WHERE w2.recordDate = w1.recordDate - INTERVAL 1 DAY)
+-- SELECT w1.id
+-- FROM weather w1
+-- WHERE w1.temperature >
+-- (SELECT temperature 
+-- FROM weather w2
+-- WHERE w2.recordDate = w1.recordDate - INTERVAL 1 DAY)
+
+
+with cte AS (
+
+    SELECT id, recordDate, temperature,
+        LAG(temperature) OVER(ORDER BY recordDate) as prev_temp,
+        LAG(recordDate) OVER(ORDER BY recordDate) as prev_date
+    FROM weather
+
+)
+SELECT id
+FROM cte
+WHERE temperature > prev_temp AND recordDate = prev_date + INTERVAL 1 DAY
