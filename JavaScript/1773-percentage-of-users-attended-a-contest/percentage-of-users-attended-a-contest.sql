fdde5987068c0a59c5a69fcf24819fa20625c320
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-05-11 21:02:14 -0400

    Time: 2553 ms (74.73%) | Memory: 0B (100.00%) - LeetSync

--- JavaScript/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ JavaScript/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,12 +1,17 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
+-- WITH total_users AS (
+-- SELECT SUM(1) as user_count FROM users)
+-- SELECT 
+-- contest_id , ROUND(count(user_id) * 100.0 / user_count , 2) as percentage
+
+-- FROM register 
+-- CROSS JOIN total_users
+-- GROUP BY contest_id
+-- ORDER BY percentage DESC, contest_id 
 
 
-WITH total_users AS (
-SELECT SUM(1) as user_count FROM users)
-SELECT 
-contest_id , ROUND(count(user_id) * 100.0 / user_count , 2) as percentage
-
-FROM register 
-CROSS JOIN total_users
+SELECT contest_id, ROUND(count(*) * 100.0/ (SELECT SUM(1) as user_count FROM users), 2) AS percentage
+FROM register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id 
+
