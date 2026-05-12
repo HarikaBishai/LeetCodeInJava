9228c2b803432999724f772e407910298461768a
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-05-12 02:35:42 -0400

    Time: 1267 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- JavaScript/180-consecutive-numbers/consecutive-numbers.sql
+++ JavaScript/180-consecutive-numbers/consecutive-numbers.sql
@@ -10,9 +10,27 @@
 -- WHERE num = prev AND prev = prev1
 
 
-SELECT DISTINCT(l1.num) as ConsecutiveNums
-FROM logs l1
-JOIN logs l2
-ON l1.id = l2.id - 1 AND l1.num = l2.num
-JOIN logs l3
-ON l2.id = l3.id - 1 AND l2.num = l3.num
+-- SELECT DISTINCT(l1.num) as ConsecutiveNums
+-- FROM logs l1
+-- JOIN logs l2
+-- ON l1.id = l2.id - 1 AND l1.num = l2.num
+-- JOIN logs l3
+-- ON l2.id = l3.id - 1 AND l2.num = l3.num
+
+
+
+WITH numbered AS (
+    SELECT id, num,
+    ROW_NUMBER() OVER (ORDER BY id) as rn_all,
+    ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) as rn_num
+    FROM logs
+),
+grouped AS (
+SELECT num, rn_all - rn_num as grp
+FROM numbered
+)
+
+SELECT DISTINCT num as ConsecutiveNums
+FROM grouped
+GROUP BY num, grp
+HAVING count(*) >= 3
