43fe58bad433783d3f2fedf152202d11e1a66695
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-06-13 19:22:54 -0400

    Time: 6 ms (12.25%) | Memory: 49.9 MB (28.48%) - LeetSync

---
+++ JavaScript/257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    List<String> out = new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        getPath(root, "");
+        return out;
+    }
+
+    public void getPath(TreeNode root, String path) {
+        if(root == null) {
+            return;
+        }
+
+        path +=  root.val;
+       
+        if(root.left==null && root.right==null) {
+            out.add(path);
+            return;
+        }
+
+        if(root.left!=null) {
+            getPath(root.left,path + "->" );
+        }
+        if(root.right!=null) {
+            getPath(root.right,path + "->" );
+        }
+    }
+}
