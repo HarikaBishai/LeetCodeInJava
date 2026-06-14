69691818dd1878510b6f607169f6dd3c7136fb29
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-06-13 21:30:58 -0400

    Time: 5 ms (28.79%) | Memory: 54.7 MB (5.90%) - LeetSync

--- JavaScript/200-number-of-islands/number-of-islands.java
+++ JavaScript/200-number-of-islands/number-of-islands.java
@@ -2,17 +2,19 @@ class Solution {
     private int ROWS;
     private int COLS;
     private int islands = 0;
-    private Set<String> visited = new HashSet<>();
+    private boolean[][] visited;
     private char[][] grid;
     public int numIslands(char[][] grid) {
         ROWS = grid.length;
         COLS = grid[0].length;
         this.grid = grid;
+
+        visited = new boolean[ROWS][COLS];
         
         for(int i=0;i<ROWS;i++) {
             for(int j=0;j<COLS;j++) {
-                String curr = String.valueOf(i)+ '_' + String.valueOf(j);
-                if(this.grid[i][j] == '1' && !visited.contains(curr)) {
+               
+                if(this.grid[i][j] == '1' && !visited[i][j]) {
                     islands++;
                     dfs(i, j);
                 }
@@ -24,16 +26,16 @@ class Solution {
 
     public void dfs(int i, int j) {
         
-        visited.add(String.valueOf(i)+ '_' + String.valueOf(j));
+        visited[i][j] = true;
 
         int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
         for (int[] dir: directions){
             int new_r = i + dir[0];
             int new_c = j + dir[1];
-            String curr = String.valueOf(new_r)+ '_' + String.valueOf(new_c);
+           
 
-            if(new_r >= 0 && new_r < ROWS && new_c >= 0 && new_c <COLS && !visited.contains(curr) && grid[new_r][new_c] == '1') {
+            if(new_r >= 0 && new_r < ROWS && new_c >= 0 && new_c <COLS && !visited[new_r][new_c] && grid[new_r][new_c] == '1') {
                 dfs(new_r, new_c);
             }
         }
