c5d1454ec1c3463d9ad11b75ed6e9f47a54bbba3
Harika Bishai <60469239+HarikaBishai@users.noreply.github.com>
2026-06-14 01:56:45 -0400

    Time: 1 ms (100.00%) | Memory: 43.5 MB (46.39%) - LeetSync

--- JavaScript/3312-number-of-changing-keys/number-of-changing-keys.java
+++ JavaScript/3312-number-of-changing-keys/number-of-changing-keys.java
@@ -2,10 +2,10 @@ class Solution {
     public int countKeyChanges(String s) {
         int count = 0;
 
-        s = s.toLowerCase();
+
 
         for(int i=1; i<s.length(); i++) {
-            if(s.charAt(i-1) != s.charAt(i)) {
+            if(Character.toLowerCase(s.charAt(i-1)) != Character.toLowerCase(s.charAt(i))) {
                 count++;
             }
         }
